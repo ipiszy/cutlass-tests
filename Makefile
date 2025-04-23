4b93057f78b659a137f5d7da3a76a5d6e7b26956
Ying Zhang <ipiszy@gmail.com>
2025-04-23 15:23:50 -0700

    loop unrolling behavior

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 # Set CUDA installation path (modify if needed)
-CUDA_HOME ?= /usr/local/cuda-12.6
+CUDA_HOME ?= /usr/local/cuda-12.8
 NVCC := $(CUDA_HOME)/bin/nvcc
 
 
 # Compiler flags
-CUDA_ARCH := -gencode arch=compute_90a,code=sm_90a  # Adjust for your GPU
+# CUDA_ARCH := -gencode arch=compute_90a,code=sm_90a  # Adjust for your GPU
+CUDA_ARCH := -gencode arch=compute_100a,code=sm_100a  # Adjust for your GPU
 CUDA_FLAGS := -std=c++17 $(CUDA_ARCH) -lineinfo \
               --use_fast_math \
               --resource-usage \
@@ -14,8 +15,8 @@ CUDA_FLAGS := -std=c++17 $(CUDA_ARCH) -lineinfo \
               -DCUTE_SM90_EXTENDED_MMA_SHAPES_ENABLED \
               -DCUTLASS_ENABLE_GDC_FOR_SM90 \
               -DNDEBUG \
-	      --debug --device-debug
-              # -O3 \
+              -O3
+	      # --debug --device-debug \
               # --ftemplate-backtrace-limit=0  \ # To debug template code
               # --keep \
               # --ptxas-options=--verbose --register-usage-level=5 --warn-on-local-memory-usage \  # printing out number of registers
@@ -33,7 +34,7 @@ LIBS := -lcudart -lcublas
 
 
 # Binaries
-BINARIES := $(BUILD_DIR)/layout $(BUILD_DIR)/mem_latency $(BUILD_DIR)/cpasync
+BINARIES := $(BUILD_DIR)/layout $(BUILD_DIR)/mem_latency $(BUILD_DIR)/cpasync $(BUILD_DIR)/loop_unrolling
 
 
 # Default target
@@ -51,6 +52,9 @@ $(BUILD_DIR)/mem_latency: $(BUILD_DIR)/mem_latency.o
 
 $(BUILD_DIR)/cpasync: $(BUILD_DIR)/cp_async.o
 	$(NVCC) $(BUILD_DIR)/cp_async.o -o $@ $(LIB_DIRS) $(LIBS)
+
+$(BUILD_DIR)/loop_unrolling: $(BUILD_DIR)/loop_unrolling.o
+	$(NVCC) $(BUILD_DIR)/loop_unrolling.o -o $@ $(LIB_DIRS) $(LIBS)
 
 
 # Clean up
---
+++ src/loop_unrolling.cu
@@ -0,0 +1,251 @@
+/**
+I observed a case that loop-unrolling causes huge register spills when the loop modifies a variable inside the loop.
+The purpose of this test is to reproduce this behavior.
+
+Reported by ptxas:
+ptxas info    : Compiling entry function '_Z27loop_unrolling_complex_goodPfS_S_' for 'sm_100a'
+ptxas info    : Function properties for _Z27loop_unrolling_complex_goodPfS_S_
+    0 bytes stack frame, 0 bytes spill stores, 0 bytes spill loads
+ptxas info    : Used 32 registers, used 0 barriers
+ptxas info    : Compile time = 20.092 ms
+ptxas info    : Compiling entry function '_Z26loop_unrolling_complex_badPfS_S_' for 'sm_100a'
+ptxas info    : Function properties for _Z26loop_unrolling_complex_badPfS_S_
+    1024 bytes stack frame, 0 bytes spill stores, 0 bytes spill loads
+ptxas info    : Used 32 registers, used 0 barriers, 1024 bytes cumulative stack size
+ptxas info    : Compile time = 36.344 ms
+
+The "good" loop interleaves LDG and other computations. The "bad" loop tries to do all LDGs at the beginning and 
+move data from registers to local memory.
+**/
+
+#include <iostream>
+
+#include "cute/arch/simd_sm100.hpp"
+#include <cute/tensor.hpp>
+#include "cutlass/array.h"
+#include "cutlass/layout/layout.h"
+#include "cutlass/numeric_conversion.h"
+#include "cutlass/util/helper_cuda.hpp"
+#include "cutlass/util/host_tensor.h"
+#include "cutlass/util/reference/device/tensor_fill.h"
+
+using namespace cute;
+
+__global__ void loop_unrolling_complex_bad(float* in0, float* in1, float* res) {
+  auto arr_SF_P_float = cute::make_tensor<float>(Int<4>{});
+  auto tTMEM_LOADrS = cute::make_tensor<float>(Int<128>{});
+  auto res_array = cute::make_tensor<int>(Int<128>{});
+
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 4; ++i) {
+    arr_SF_P_float[i] = in0[i];
+  }
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 128; ++i) {
+    tTMEM_LOADrS[i] = in1[i];
+  }
+
+  const int inner_n = 32;
+  const int outer_n = size(tTMEM_LOADrS) / inner_n;
+
+  float2 minus_row_max_scale_fp32x2 = make_float2(-0.34, -0.34);
+  float2 scale_fp32x2 = make_float2(-0.45, -0.45);
+
+  cutlass::NumericConverter<float, int> convert;
+
+  int offset_i = 0;
+
+  CUTLASS_PRAGMA_UNROLL
+  for (int offset = 0, outer_i = 0; offset < size(tTMEM_LOADrS); offset += inner_n, outer_i += 1) {
+    float2 modified_scale;
+    cute::add(
+      modified_scale, minus_row_max_scale_fp32x2, 
+      make_float2(-arr_SF_P_float[outer_i], -arr_SF_P_float[outer_i])
+    );
+    CUTLASS_PRAGMA_UNROLL
+    for (; offset_i < offset + inner_n; offset_i += 2) {
+      float2 in = make_float2(
+        tTMEM_LOADrS(offset_i + 0),
+        tTMEM_LOADrS(offset_i + 1)
+      );
+      float2 out;
+      cute::fma(out, scale_fp32x2, in, modified_scale);
+      tTMEM_LOADrS(offset_i + 0) = out.x;
+      tTMEM_LOADrS(offset_i + 1) = out.y;
+  
+      tTMEM_LOADrS(offset_i + 0) = ::exp2f(tTMEM_LOADrS(offset_i + 0));
+      tTMEM_LOADrS(offset_i + 1) = ::exp2f(tTMEM_LOADrS(offset_i + 1));
+  
+      res_array[(offset_i)] = convert(tTMEM_LOADrS(offset_i));
+      res_array[(offset_i+1)] = convert(tTMEM_LOADrS(offset_i+1));
+    }
+  }
+
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 128; ++i) {
+    *res += res_array[i];
+  }
+}
+
+__global__ void loop_unrolling_complex_good(float* in0, float* in1, float* res) {
+  auto arr_SF_P_float = cute::make_tensor<float>(Int<4>{});
+  auto tTMEM_LOADrS = cute::make_tensor<float>(Int<128>{});
+  auto res_array = cute::make_tensor<int>(Int<128>{});
+
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 4; ++i) {
+    arr_SF_P_float[i] = in0[i];
+  }
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 128; ++i) {
+    tTMEM_LOADrS[i] = in1[i];
+  }
+
+  const int inner_n = 32;
+  const int outer_n = size(tTMEM_LOADrS) / inner_n;
+
+  float2 minus_row_max_scale_fp32x2 = make_float2(-0.34, -0.34);
+  float2 scale_fp32x2 = make_float2(-0.45, -0.45);
+
+  cutlass::NumericConverter<float, int> convert;
+
+  CUTLASS_PRAGMA_UNROLL
+  for (int offset = 0, outer_i = 0; offset < size(tTMEM_LOADrS); offset += inner_n, outer_i += 1) {
+    float2 modified_scale;
+    cute::add(
+      modified_scale, minus_row_max_scale_fp32x2, 
+      make_float2(-arr_SF_P_float[outer_i], -arr_SF_P_float[outer_i])
+    );
+    CUTLASS_PRAGMA_UNROLL
+    for (int i = 0; i < inner_n; i += 2) {
+      float2 in = make_float2(
+        tTMEM_LOADrS(offset+i + 0),
+        tTMEM_LOADrS(offset+i + 1)
+      );
+      float2 out;
+      cute::fma(out, scale_fp32x2, in, modified_scale);
+      tTMEM_LOADrS(offset+i + 0) = out.x;
+      tTMEM_LOADrS(offset+i + 1) = out.y;
+  
+      tTMEM_LOADrS(offset+i + 0) = ::exp2f(tTMEM_LOADrS(offset+i + 0));
+      tTMEM_LOADrS(offset+i + 1) = ::exp2f(tTMEM_LOADrS(offset+i + 1));
+  
+      res_array[(offset+i)] = convert(tTMEM_LOADrS(offset+i));
+      res_array[(offset+i+1)] = convert(tTMEM_LOADrS(offset+i+1));
+    }
+  }
+
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 128; ++i) {
+    *res += res_array[i];
+  }
+}
+
+
+/**
+__global__ void loop_unrolling_good(float* in0, float* in1, float* res)
+{
+  const int outer_n = 128;
+  const int inner_n = 32;
+
+  *res = 0;
+  auto in0_t = cute::make_tensor<float>(Int<4>{});
+  auto in1_t = cute::make_tensor<float>(Int<128>{});
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 4; ++i) {
+    in0_t[i] = in0[i];
+  }
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 128; ++i) {
+    in1_t[i] = in1[i];
+  }
+
+  CUTLASS_PRAGMA_UNROLL
+  for (int offset = 0, outer_i = 0; offset < outer_n; offset += inner_n, outer_i += 1) {
+    float modified_scale = outer_i * in0_t[outer_i];
+
+    CUTLASS_PRAGMA_UNROLL
+    for (int i = 0; i < inner_n; i += 2) {
+      *res += modified_scale * in1_t[i + offset];
+    }
+  }
+}
+
+__global__ void loop_unrolling_bad(float* in0, float* in1, float* res)
+{
+  const int outer_n = 128;
+  const int inner_n = 32;
+
+  *res = 0;
+  int offset_i = 0;
+  auto in0_t = cute::make_tensor<float>(Int<4>{});
+  auto in1_t = cute::make_tensor<float>(Int<128>{});
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 4; ++i) {
+    in0_t[i] = in0[i];
+  }
+  CUTLASS_PRAGMA_UNROLL
+  for (int i = 0; i < 128; ++i) {
+    in1_t[i] = in1[i];
+  }
+
+  CUTLASS_PRAGMA_UNROLL
+  for (int offset = 0, outer_i = 0; offset < outer_n; offset += inner_n, outer_i += 1) {
+    float modified_scale = outer_i * in0_t[outer_i];
+
+    CUTLASS_PRAGMA_UNROLL
+    for (; offset_i < offset + inner_n; offset_i += 2) {
+      *res += modified_scale * in1_t[offset_i];
+    }
+  }
+}
+**/
+
+
+int main(int argc, char** argv)
+{
+  double mean = 0.5;
+  double stddev = 2.0;
+  uint64_t seed = 0x2019;
+
+  cutlass::HostTensor<float, cutlass::layout::RowMajor> in_tensor0({4, 1});
+  cutlass::HostTensor<float, cutlass::layout::RowMajor> in_tensor1({128, 1});
+  // Initialize in device memory
+  cutlass::reference::device::TensorFillRandomGaussian(
+    in_tensor0.device_view(),
+    seed,
+    (float)(mean),
+    (float)(stddev));
+  cutlass::reference::device::TensorFillRandomGaussian(
+    in_tensor1.device_view(),
+    seed,
+    (float)(mean),
+    (float)(stddev));
+
+  cutlass::HostTensor<float, cutlass::layout::RowMajor> tensor({1, 1});
+
+  // // Test loop unrolling - low register usage case.
+  // loop_unrolling_good<<<1, 1>>>(in_tensor0.device_data(), in_tensor1.device_data(), tensor.device_data());
+  // tensor.sync_host();
+  // std::cout << "good, res: " << tensor.host_data()[0] << std::endl;
+  // CUTE_CHECK_LAST();
+
+  // // Test loop unrolling - high register usage case.
+  // loop_unrolling_bad<<<1, 1>>>(in_tensor0.device_data(), in_tensor1.device_data(), tensor.device_data());
+  // tensor.sync_host();
+  // std::cout << "bad, res: " << tensor.host_data()[0] << std::endl;
+  // CUTE_CHECK_LAST();
+
+  // Test loop unrolling - complex usage case.
+  loop_unrolling_complex_bad<<<1, 1>>>(in_tensor0.device_data(), in_tensor1.device_data(), tensor.device_data());
+  tensor.sync_host();
+  std::cout << "complex bad, res: " << tensor.host_data()[0] << std::endl;
+  CUTE_CHECK_LAST();
+
+  loop_unrolling_complex_good<<<1, 1>>>(in_tensor0.device_data(), in_tensor1.device_data(), tensor.device_data());
+  tensor.sync_host();
+  std::cout << "complex good, res: " << tensor.host_data()[0] << std::endl;
+  CUTE_CHECK_LAST();
+
+  return 0;
+}
